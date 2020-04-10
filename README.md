a00f833844894e50e7505ce7782f42acdc6b76aa
Deepam Gupta <deepam.deepam.gupta@gmail.com>
2020-04-10 23:32:44 +0530

    Updated Readme

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # trip_cost_app
-Calculates the cost of your trip according to the fuel consumed
+
+This is the third app which I created while learning Flutter.
+
+## What this app does?
+
+> Claculate the cost of the net fuel you have consumed while on a trip.
+
+## Getting started -> Starting *trip_cost_app*
+
+### How to use __trip_cost_app__ ?
+
+1. Enter the distance.
+2. Enter the Per unit disctance i.e. average of the vechile.
+3. Enter the Rate of the Fuel per Litre.
+4. Select the currency.
+5. Click on Submit.
+6. The result will be printed on your screen.
+
+NAMASTE 🙏
---
+++ lib/main.dart
@@ -0,0 +1,198 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Trip Cost Calculator',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: new FuelForm(),
+    );
+  }
+}
+
+class FuelForm extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _FuelFormState();
+}
+
+class _FuelFormState extends State<FuelForm> {
+  String result = '';
+  final _currencies = ['Rupees', 'Dollars', 'Pounds'];
+  String _currency = 'Rupees';
+  TextEditingController distanceController = TextEditingController();
+  TextEditingController avgController = TextEditingController();
+  TextEditingController priceController = TextEditingController();
+  final double _formDistance = 5.0;
+
+  @override
+  Widget build(BuildContext context) {
+    TextStyle textStyle = Theme.of(context).textTheme.title;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Hello"),
+        backgroundColor: Colors.blueAccent,
+      ),
+      body: Container(
+        padding: EdgeInsets.all(15.0),
+        child: Column(
+          children: <Widget>[
+            Padding(
+              padding:
+                  EdgeInsets.only(top: _formDistance, bottom: _formDistance),
+              child: TextField(
+                controller: distanceController,
+                decoration: InputDecoration(
+                    labelText: 'Distance',
+                    hintText: 'e.g. 124',
+                    labelStyle: textStyle,
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(5.0))),
+                keyboardType: TextInputType.number,
+
+                // We do not need onchenged method as TextEditingController will take care of it
+                // onChanged: (String string) {
+                //   setState(() {
+                //     name = string;
+                //   });
+                // },
+              ),
+            ),
+            Padding(
+              padding:
+                  EdgeInsets.only(top: _formDistance, bottom: _formDistance),
+              child: TextField(
+                controller: avgController,
+                decoration: InputDecoration(
+                    labelText: 'Distance per Unit',
+                    hintText: 'e.g. 17',
+                    labelStyle: textStyle,
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(5.0))),
+                keyboardType: TextInputType.number,
+
+                // We do not need onchenged method as TextEditingController will take care of it
+                // onChanged: (String string) {
+                //   setState(() {
+                //     name = string;
+                //   });
+                // },
+              ),
+            ),
+            Padding(
+                padding:
+                    EdgeInsets.only(top: _formDistance, bottom: _formDistance),
+                child: Row(
+                  children: <Widget>[
+                    Expanded(
+                      child: TextField(
+                        controller: priceController,
+                        decoration: InputDecoration(
+                            labelText: 'Price',
+                            hintText: 'e.g. 1.54',
+                            labelStyle: textStyle,
+                            border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(5.0))),
+                        keyboardType: TextInputType.number,
+
+                        // We do not need onchenged method as TextEditingController will take care of it
+                        // onChanged: (String string) {
+                        //   setState(() {
+                        //     name = string;
+                        //   });
+                        // },
+                      ),
+                    ),
+                    Container(
+                      width: _formDistance * 5,
+                    ),
+                    Expanded(
+                      child: DropdownButton<String>(
+                        items: _currencies.map((String value) {
+                          return DropdownMenuItem(
+                            value: value,
+                            child: Text(value),
+                          );
+                        }).toList(),
+                        value: _currency,
+                        onChanged: (String value) {
+                          _onDropdownChanged(value);
+                        },
+                      ),
+                    ),
+                  ],
+                )),
+            Row(
+              children: <Widget>[
+                Expanded(
+                  child: RaisedButton(
+                    color: Theme.of(context).primaryColorDark,
+                    textColor: Theme.of(context).primaryColorLight,
+                    onPressed: () {
+                      setState(() {
+                        result = _calculate();
+                      });
+                    },
+                    child: Text(
+                      'Submit',
+                      textScaleFactor: 1.5,
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: RaisedButton(
+                    color: Theme.of(context).buttonColor,
+                    textColor: Theme.of(context).primaryColorDark,
+                    onPressed: () {
+                      setState(() {
+                        result = _reset();
+                      });
+                    },
+                    child: Text(
+                      'Reset',
+                      textScaleFactor: 1.5,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Text(result)
+          ],
+        ),
+      ),
+    );
+  }
+
+  _onDropdownChanged(String value) {
+    setState(() {
+      this._currency = value;
+    });
+  }
+
+  String _calculate() {
+    double _distance = double.parse(distanceController.text);
+    double _fuelCost = double.parse(avgController.text);
+    double _consumption = double.parse(priceController.text);
+    double _totalCost = _distance / _consumption * _fuelCost;
+    String _result = 'The total cost of your trip is ' +
+        _totalCost.toStringAsFixed(2) +
+        ' ' +
+        _currency;
+    return _result;
+  }
+
+  _reset() {
+    distanceController.text = '';
+    avgController.text = '';
+    priceController.text = '';
+    setState(() {
+      result = '';
+    });
+  }
+}
